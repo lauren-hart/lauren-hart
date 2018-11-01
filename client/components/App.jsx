02f4458f6d3a6491e23b2925816a3131bc5ad4a3
Lauren Hart <laurenannehart@gmail.com>
2018-11-02 10:52:30 +1300

    moved switch to Home.jsx

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,20 +1,12 @@
 import React from 'react'
-import {Route, Switch} from 'react-router-dom'
 
 import Home from './Home'
-import Header from './Header'
-import Footer from './Footer'
 
 class App extends React.Component {
   render () {
     return (
       <div className='app'>
-        <Header />
-        <Switch>
-          <Route exact path='/' component={Home} />
-        </Switch>
-
-        <Footer />
+        <Home />
       </div>
     )
   }
--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -4,15 +4,25 @@ import About from './About'
 import Stack from './Stack'
 import Experience from './Experience'
 import Projects from './Projects'
+import Header from './Header'
+import Footer from './Footer'
+import {Route, Switch} from 'react-router-dom'
 
 class Home extends React.Component {
   render () {
     return (
       <div>
-        <About />
-        <Stack />
-        <Experience />
-        <Projects />
+        <Header />
+
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <About path='/about' component={About} />
+          <Stack path='/stack' component={Stack} />
+          <Experience path='/experience' component={Experience} />
+          <Projects path='/projects' component={Projects} />
+        </Switch>
+
+        <Footer />
       </div>
     )
   }
